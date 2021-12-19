d998208258bb8ef23955855229ad56638d69735e
RealFaisFarid <47639443+RealFaisFarid@users.noreply.github.com>
2021-12-19 12:27:46 +0600

    reg form

---
+++ php/php_e_reg_form.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<form action="php_e_reg_form_welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
